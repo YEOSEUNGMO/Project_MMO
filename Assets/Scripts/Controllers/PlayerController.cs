f7821b569ca87a3a36ba1083888dcc239825efcd
YEOSEUNGMO <ysm2107@gmail.com>
2021-08-14 15:40:23 +0900

    first commit from laptop

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    enum PlayerState
+    {
+        Die,
+        Idle,
+        Move,
+    }
+    PlayerState _state = PlayerState.Idle;
+    float _speed = 10f;
+
+    Vector3 _destPos;
+    Animator anim = null;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        Managers.Input.MouseAction -= OnMouseClicked;
+        Managers.Input.MouseAction += OnMouseClicked;
+    }
+
+    public void UpdateMoving()
+    {
+        Vector3 dir = _destPos - transform.position;
+        if (dir.magnitude < 0.00001f)
+        {
+            _state = PlayerState.Idle;
+        }
+        else
+        {
+            float moveDist = Mathf.Clamp(Time.deltaTime * _speed, 0, dir.magnitude);
+            transform.position += (dir.normalized * moveDist);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 20 * Time.deltaTime);
+        }
+
+        //animation
+        anim.SetFloat("speed", _speed);
+    }
+
+    public void UpdateIdle()
+    {
+        anim.SetFloat("speed", 0);
+    }
+
+    void Update()
+    {
+        switch(_state)
+        {
+            case PlayerState.Die:
+                break;
+            case PlayerState.Move:
+                UpdateMoving();
+                break;
+            case PlayerState.Idle:
+                UpdateIdle();
+                break;
+        }
+    }
+
+    public void OnKeyboard()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
+            transform.position += (Vector3.forward * Time.deltaTime * _speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
+            transform.position += (Vector3.back * Time.deltaTime * _speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
+            transform.position += (Vector3.left * Time.deltaTime * _speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
+            transform.position += (Vector3.right * Time.deltaTime * _speed);
+        }
+    }
+
+    public void OnMouseClicked(Define.MouseEvent mouseEvent)
+    {
+        if (_state == PlayerState.Die)
+            return;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        LayerMask mask = LayerMask.GetMask("Floor");
+        RaycastHit hit;
+        if(Physics.Raycast(ray,out hit,100.0f,mask))
+        {
+            _destPos = hit.point;
+            _state = PlayerState.Move;
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_instance = null;
+    public static Managers Instantance { get{ Init(); return s_instance; } }
+
+    InputManager _input = new InputManager();
+    public static InputManager Input { get { return Instantance._input; } }
+
+    ResourceManager _resource = new ResourceManager();
+    public static ResourceManager Resource { get { return Instantance._resource; } }
+
+    private void Awake()
+    {
+        Init();
+    }
+    public static void Init()
+    {
+        if(s_instance == null)
+        {
+            GameObject obj = GameObject.Find("@Managers");
+            if (obj == null)
+            {
+                obj = new GameObject { name = "@Managers" };
+                obj.AddComponent<Managers>();
+            }
+            DontDestroyOnLoad(obj);
+            s_instance = obj.GetComponent<Managers>();
+        }
+    }
+
+    void Update()
+    {
+        _input.OnUpdate();
+    }
+}
